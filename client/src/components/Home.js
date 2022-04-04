5c4eaa61643044da42dbd1d828f88f2e453e7d8c
Aidan Welch <aidan@freedwave.com>
2022-04-04 05:15:36 -0400

    Fix addMessageToConversation, AddNewConvo add prev Properly implemented setConversations in both functions to use the prev. Follows the same style as addOnlineUser and other functions.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -80,14 +80,18 @@ const Home = ({ user, logout }) => {
 
   const addNewConvo = useCallback(
     (recipientId, message) => {
-      conversations.forEach((convo) => {
-        if (convo.otherUser.id === recipientId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-          convo.id = message.conversationId;
-        }
-      });
-      setConversations(conversations);
+	  setConversations((prev) =>
+	    prev.map((convo) => {
+		  if (convo.otherUser.id === recipientId) {
+			const convoCopy = { ...convo };
+			convoCopy.messages = [ message, ...convo.messages ];
+			convoCopy.id = message.conversationId;
+			return convoCopy;
+		  } else {
+			return convo;
+		  }
+		})
+	  )
     },
     [setConversations, conversations],
   );
@@ -104,15 +108,20 @@ const Home = ({ user, logout }) => {
         };
         newConvo.latestMessageText = message.text;
         setConversations((prev) => [newConvo, ...prev]);
-      }
-
-      conversations.forEach((convo) => {
-        if (convo.id === message.conversationId) {
-          convo.messages.push(message);
-          convo.latestMessageText = message.text;
-        }
-      });
-      setConversations(conversations);
+      } else {
+		setConversations((prev) => 
+		  prev.map((convo) => {
+			if (convo.id === message.conversationId) {
+			  const convoCopy = { ...convo };
+			  convoCopy.messages = [ message, ...convo.messages ];
+			  convoCopy.latestMessageText = message.text;
+			  return convoCopy;
+			} else {
+			  return convo;
+			}
+		  })
+		)
+	  }
     },
     [setConversations, conversations],
   );
