16f7411ccb4fc65c3771f966d45c2791041ecd99
Aidan Welch <aidan@freedwave.com>
2022-04-14 02:54:17 -0400

    implement clear-unread socket in views.py

--- server/socketio_app/views.py
+++ server/socketio_app/views.py
@@ -4,6 +4,7 @@ import os
 
 import socketio
 from online_users import online_users
+from messenger_backend.models import Conversation
 
 basedir = os.path.dirname(os.path.realpath(__file__))
 sio = socketio.Server(async_mode=async_mode, logger=False)
@@ -36,3 +37,15 @@ def logout(sid, user_id):
     if user_id in online_users:
         online_users.remove(user_id)
     sio.emit("remove-offline-user", user_id, skip_sid=sid)
+
+
+@sio.on("clear-unread")
+def clear_unread(sid, other_user_id, conversation_id):
+	conversation = Conversation.objects.filter(id=conversation_id).first() if conversation_id else None
+	if conversation == None: 
+		return
+	if conversation.user1 and conversation.user1.id != other_user_id:
+		conversation.user1Unread = 0
+	elif conversation.user2 and conversation.user2.id != other_user_id:
+		conversation.user2Unread = 0
+	conversation.save()
