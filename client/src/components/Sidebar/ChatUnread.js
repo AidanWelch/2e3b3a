f157f417126b42c9bc0c22293976f806a0ec07dd
Aidan Welch <aidan@freedwave.com>
2022-04-14 02:46:55 -0400

    Add the unread badge "if(!unread) return null" is used because it is still visible on 0

---
+++ client/src/components/Sidebar/ChatUnread.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Box, Badge } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+	marginRight: 35,
+	height: 20
+  },
+}));
+
+const ChatUnread = ({ conversation }) => {
+  const classes = useStyles();
+
+  const unread = conversation.unread;
+
+  if(!unread)
+	return null;
+
+  return (
+    <Badge
+	  badgeContent={unread}
+	  color="primary"
+	  anchorOrigin={{ horizontal: "right", vertical: "center" }}
+	  transformOrigin={{ horizontal: "left", vertical: "center" }}	
+	  className={classes.root}
+	  invisible={true}
+	/>
+  );
+};
+
+export default ChatUnread;
